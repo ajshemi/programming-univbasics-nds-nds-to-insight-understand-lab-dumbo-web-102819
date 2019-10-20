77a254deeaa530aa01ea6e82be26b9f99025bd68
essential-hacker-4489 <akpojotor@yahoo.com>
2019-10-20 09:29:14 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,20 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
+  pp nds
   nil
 end
 
-def print_first_directors_movie_titles
+def print_first_directors_movie_titles()
+  nds=directors_database()
+  nds.map do |n|
+    if n[:name]="Stephen Spielberg"
+      puts n[:movies][0]
+    end
+    #if n[:name]="Stephen Spielberg"
+      #n[:movies].map do |array|
+      #  puts array[:title]
+      #end
+    #end
+  end
 end
